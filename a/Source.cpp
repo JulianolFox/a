e7e06648b2f2843ed700e43c1df991cf80b3b631
OiTudoBom <julia@DESKTOP-CG9ISB4>
2017-09-14 18:58:34 -0300

    PAUNOCU

--- a/Source.cpp
+++ a/Source.cpp
@@ -6,6 +6,7 @@ int main() {
 	int a = 0;
 	cout << "Nota:" << a << endl << "vtnc" << endl;
 	cout << "2+2=3" << endl;
+	cout << "Ponteiro ponteiro ponteiro ponteiro ponteiro ponteiro" << endl;
 	system("pause");
 	return 0;
 }
